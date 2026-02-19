b1c739f2372ff858658b36c9a938ad54080affb4
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2026-02-19 12:20:39 -0500

    Signal Bus

---
+++ signal_bus.gd
@@ -0,0 +1,7 @@
+extends Node
+
+## HUD stuff
+## Player director actions
+signal player_hand_changed(hand: Array[Action])
+signal player_held_action_changed(action: Action)
+signal player_selected_actor_changed(actor: Actor)
