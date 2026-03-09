341aef409d3ecb92781c035bf9c14d242bc7225e
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2026-03-09 02:17:32 -0400

    feat: Bleed status

--- status-effects/affect_damage_dealt.gd
+++ status-effects/affect_damage_dealt.gd
@@ -1,4 +1,4 @@
-class_name StatusDamageDealt extends Status
+class_name StatusAffectDamage extends Status
 
 enum Operation {
 	SUM, ## Add or subtract
@@ -6,13 +6,48 @@ enum Operation {
 	DIVIDE,
 }
 
+enum DamageHook {
+	DEALING,
+	RECEIVING,
+}
+
+@export var incoming_or_outgoing: DamageHook = DamageHook.DEALING ## Pick the appropriate scenario
+@export var direct_only: bool = false
+
 @export var factor: float = -1.0
 @export var operation: Operation = Operation.SUM
 @export var per_point: bool = true ## NOTE Don't use this with multiply or divide unless you know what you're doing.
 
+@export var expend_points_when_applied: bool = false
+
+
 func on_deal_damage(damage: int) -> int:
+	if (not incoming_or_outgoing == DamageHook.DEALING) or direct_only:
+		return super(damage)
+	else:
+		return modify_damage(damage)
+
+func on_deal_direct_damage(damage: int) -> int:
+	if (not incoming_or_outgoing == DamageHook.DEALING) or not direct_only:
+		return super(damage)
+	else:
+		return modify_damage(damage)
+		
+func on_take_damage(damage: int) -> int:
+	if (not incoming_or_outgoing == DamageHook.RECEIVING) or direct_only:
+		return super(damage)
+	else:
+		return modify_damage(damage)
+
+func on_take_direct_damage(damage: int) -> int:
+	if (not incoming_or_outgoing == DamageHook.RECEIVING) or not direct_only:
+		return super(damage)
+	else:
+		return modify_damage(damage)
+
+
+func modify_damage(damage:int) -> int:
 	var new_damage: float = damage
-	
 	match operation:
 		Operation.SUM:
 			new_damage += factor if not per_point else factor * effect_points
@@ -22,4 +57,8 @@ func on_deal_damage(damage: int) -> int:
 		Operation.DIVIDE:
 			## NOTE Bad idea to use this with per-point
 			new_damage /= factor if not per_point else factor * effect_points
+	
+	if expend_points_when_applied:
+		pass
+	
 	return int(new_damage)
