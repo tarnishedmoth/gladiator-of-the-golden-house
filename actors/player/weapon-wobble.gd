0365ca344f9a3a4842bb6dbb518267e705c955c8
Christer Kaitila <breakdance@mcfunkypants.com>
2026-03-31 13:04:46 -0700

    sword sprites slowly bob and and down

---
+++ actors/player/weapon-wobble.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+# wobbles a sprite up and down
+
+@export var speed := 5.0
+@export var amplitude := 4.0
+var original_y: float
+
+func _ready():
+	original_y = position.y
+
+func _process(delta):
+	var new_y = original_y + (sin(Time.get_ticks_msec() / 1000.0 * speed) * amplitude)
+	position.y = new_y
