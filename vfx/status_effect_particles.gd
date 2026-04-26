56baf09fd082a18f7369ecfbd33427852521e197
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2026-04-26 13:45:35 -0400

    Defense status effect VFX particles

--- vfx/status_effect_particles.gd
+++ vfx/status_effect_particles.gd
@@ -1,3 +1,13 @@
 class_name StatusEffectParticles extends GPUParticles2D
 
+## One shot particle effects
+
 @export var use_effect_points_as_quantity: bool = true
+
+func _enter_tree() -> void:
+	emitting = false
+	one_shot = true
+
+func _ready() -> void:
+	finished.connect(queue_free)
+	restart()
