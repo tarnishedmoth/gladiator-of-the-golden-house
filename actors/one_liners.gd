7d05f5d553c747c5eb1a59bf291ffc47d236bf4b
Christer Kaitila <breakdance@mcfunkypants.com>
2026-05-05 13:25:30 -0700

    one liners

---
+++ actors/one_liners.gd
@@ -0,0 +1,86 @@
+extends Node2D
+
+@export var seconds_before_starting: float = 5.0
+@export var bubble_timespan_seconds: float = 2.0
+@export var wait_seconds_min: float = 5.0
+@export var wait_seconds_max: float = 10.0
+
+@export var one_liners: Array[String] = [
+	"Excelcior!",
+	"Prepare to die!",
+	"Prepare yourself.",
+	"For the emperor!",
+	"I do this for honor.",
+	"Our names will become legend.",
+	"Darkness consumes me.",
+	"I do not fear death.",
+	"I see storms on the horizon!",
+	"I shall cut you down.",
+	"For glory!",
+	"Imperium vitae!",
+	"Deus Vult!", # god wills it in latin
+	"Signa inferre!", # signals forward latin
+	"Mars Ultor!", # god of war the avenger
+	"Desperta Ferro!", # Awaken Iron!
+	"Si vis pacem, para bellum", # If you want peace, prepare for war
+	"Omnia vincit amor.", # Love conquers all - often used ironically
+	"Ad astra per aspera!", # To the stars through difficulties
+	"Morituri te salutant.", # those who are about to die salute you
+	"Silence!",
+	"Respect!",
+	"Glory!",
+	"Honor!",
+	"Victory!",
+	"Today is a good day to die!",
+	"Are you not entertained?",
+	"Strength and honor!",
+	"What we do in life echoes in eternity.",
+	"Tennoheika Banzai!", # Japanese for 10,000 years! (long live the emperor)
+	"Oorah!",
+	"I shall defeat you!",
+	]
+
+var one_liner_delay_remaining:float = 0.0
+var bubble_time_remaining:float = 0.0
+
+############################################
+# you can call this function from anywhere #
+# for example, in response to getting hit! #
+############################################
+func say_this_oneliner(bark:String):
+	print("one liner: "+bark)
+	# show the bubble for a while
+	bubble_time_remaining = bubble_timespan_seconds
+	# plus a random delay afterwards before barks continue
+	one_liner_delay_remaining = randf_range(wait_seconds_min, wait_seconds_max)
+	# change the text
+	$one_liner_txt.text = bark
+	# make bubble visible
+	show()
+
+func _ready():
+	randomize()
+	hide() # the bubble to begin with
+	# wait before starting random oneliners
+	one_liner_delay_remaining = seconds_before_starting
+	# extra so they don't start at the same time
+	one_liner_delay_remaining += randf_range(wait_seconds_min, wait_seconds_max)
+
+func display_random_oneliner():
+	# choose one randomly
+	# TODO avoid repeats? go in sequence+loop from a random start index?
+	var str = one_liners[randi() % one_liners.size()]
+	say_this_oneliner(str)
+
+func _process(delta):
+	
+	bubble_time_remaining -= delta
+	if (bubble_time_remaining > 0.0):
+		return # keep showing it
+	else:
+		hide() # the bubble
+	
+	one_liner_delay_remaining -= delta
+	if one_liner_delay_remaining <= 0.0:
+		display_random_oneliner()
+		
