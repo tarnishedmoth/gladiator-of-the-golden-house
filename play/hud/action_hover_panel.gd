1cc347f84d2742e37a1ceb207c77682e7f6c6c3f
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2026-05-07 20:39:01 -0400

    Action hover panel displays category

--- play/hud/action_hover_panel.gd
+++ play/hud/action_hover_panel.gd
@@ -2,6 +2,7 @@ class_name HUDActionHoverPanel extends PanelContainer
 
 @export var title: RichTextLabel
 @export var action_image: TextureRect
+@export var category: RichTextLabel
 @export var description: RichTextLabel
 @export var energy_cost: RichTextLabel
 @export var amount_text: RichTextLabel
@@ -10,6 +11,7 @@ class_name HUDActionHoverPanel extends PanelContainer
 
 func clear_all() -> void:
 	title.text = ""
+	category.text = ""
 	energy_cost.text = ""
 	description.text = ""
 	amount_text.text = ""
@@ -21,9 +23,11 @@ func populate_using_action_data(action:Action)->void:
 		title.text = "[center]"
 		title.append_text(TextUtils.bold(action.ui_title))
 	
+	category.text = action.ui_category
+	
+	#description.text = "[center]"
 	if action.ui_description:
-		#description.text = "[center]"
-		description.text = action.ui_description
+		description.text += "\n" + action.ui_description
 	
 	if action.ui_icon:
 		action_image.texture = action.ui_icon
@@ -37,7 +41,12 @@ func populate_using_action_data(action:Action)->void:
 		amount_text.append_text("Damage: %d " % [action.damage])
 	
 	if action is ActionApplyStatus:
-		amount_text.text = "[center]"
 		if action.status:
-			amount_text.append_text("Amount: %d" % [action.status.effect_points])
+			amount_text.text = "[center]"
+			if action.status.effect_points:
+				amount_text.append_text("Amount: %d" % [action.status.effect_points])
+			
+			description.text += "\n" + \
+				action.status.ui_name + "\n" + \
+				action.status.ui_description
 		
