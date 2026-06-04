1e40869ceacffdef35065931e7b308c3365c860c
Michael Avrie <87274781+tarnishedmoth@users.noreply.github.com>
2026-06-04 19:06:42 -0400

    cleanup: remove unused DirectionPattern script and resources

--- actors/facing/facing_direction_pattern.gd
+++
@@ -1,8 +0,0 @@
-class_name DirectionPattern extends Resource
-
-@export var front: float = 0.0
-@export var front_right: float = 0.0 
-@export var back_right: float = 0.0
-@export var back: float = 0.0
-@export var back_left: float = 0.0
-@export var front_left: float = 0.0
