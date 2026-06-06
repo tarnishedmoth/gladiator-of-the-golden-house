197dad86de39aeb14af17ec54871d2e06dd76052
Michael Avrie <87274781+tarnishedmoth@users.noreply.github.com>
2026-06-06 15:51:55 -0400

    finalized splash screen with dev pods and godot logos

--- menus/intro/splash.gd
+++ menus/intro/splash.gd
@@ -7,7 +7,8 @@ signal finished
 @onready var studio: Sprite2D = %Studio
 @onready var title := %Title
 @onready var version := %Version
-@onready var subtext := %Subtext
+@onready var side_logos: Control = $UI/SideLogos
+@onready var margin_container: MarginContainer = $UI/MarginContainer
 
 func _ready() -> void:
 	version.text = "v" + Main.get_project_version()
@@ -16,18 +17,18 @@ func _ready() -> void:
 
 func animate() -> void:
 	Juice.fade_in(ui, Juice.SLOW)
-	var up_tween: Tween = create_tween()
-	up_tween.tween_property(studio, "position", studio.position + Vector2.UP * 60.0, 8.0).set_ease(Tween.EASE_IN)
+	var tween: Tween = create_tween()
+	tween.tween_property(margin_container, "position", margin_container.position + Vector2.UP * 60.0, 8.0).set_ease(Tween.EASE_IN)
 	
 	version.modulate = Color.TRANSPARENT
 	Juice.fade_in(studio)
 	await Juice.fade_in(title, Juice.PATIENT).finished
 	await Juice.fade_in(version, Juice.SMOOTH).finished
 	await Juice.flash(version, [0.22, 0.66]).finished
-	
+	await get_tree().create_timer(1.2).timeout
 	Juice.fade_out(studio)
 	Juice.fade_out(title)
-	Juice.fade_out(subtext)
 	Juice.fade_out(version)
+	Juice.fade_out(side_logos)
 	await Juice.fade_out(ui, Juice.SLOW).finished
 	return
