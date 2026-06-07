43c688eb1be3c10f27952f5e7c87e91587a48513
Jalent (Jason W) <williamsjasonm@hotmail.com>
2026-06-07 15:17:10 -0500

    fix: spawn chance var type issue

--- actors/ai_actor.gd
+++ actors/ai_actor.gd
@@ -185,7 +185,7 @@ func plan_action_details(action: Action, claimed_tiles: Array[Vector2i]) -> void
 
 
 func die()->void:
-	if randi_range(0,1)<drop_chance:
+	if randf():
 		var picked_item = pick_up_manager.get_weighted_random()
 		pick_up_manager.spawn_pick_up(picked_item,current_tile_coords)		
 	super()
