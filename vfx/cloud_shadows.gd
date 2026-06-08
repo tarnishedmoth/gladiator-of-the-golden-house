583adfec519a1379f25bafe5d3d98a115c54cf72
Michael Avrie <87274781+tarnishedmoth@users.noreply.github.com>
2026-06-08 05:04:58 -0400

    misc: improved clouds in colosseum levels

--- vfx/cloud_shadows.gd
+++ vfx/cloud_shadows.gd
@@ -2,13 +2,14 @@ extends Sprite2D
 
 ## Autoscrolling clouds
 
-const FADE_TIME: float = 5.0
-const OFFSET_PER_SECOND: float = 2.5
+const FADE_TIME: float = 8.0
+@export var OFFSET_PER_SECOND: float = 6.5
 const LOOP_TIME: float = 45.0
 
 var scroll_tween: Tween
 func _ready() -> void:
 	self_modulate = Color.TRANSPARENT
+	#var noise: FastNoiseLite = texture.noise
 	if scroll_tween:
 		if scroll_tween.is_running():
 			scroll_tween.kill()
@@ -16,11 +17,15 @@ func _ready() -> void:
 	scroll_tween.tween_property(self, ^"self_modulate", Color.WHITE, FADE_TIME)
 	scroll_tween.parallel()
 	scroll_tween.tween_property(self, ^"offset:x", offset.x + (OFFSET_PER_SECOND * LOOP_TIME), LOOP_TIME).from(0.0)
+	#scroll_tween.parallel()
+	#scroll_tween.tween_property(noise, ^"offset:z", (OFFSET_PER_SECOND * LOOP_TIME), LOOP_TIME).from(0.0)
 	scroll_tween.chain()
 	
 	scroll_tween.tween_property(self, ^"self_modulate", Color.TRANSPARENT, FADE_TIME)
 	scroll_tween.parallel()
 	scroll_tween.tween_property(self, ^"offset:x", OFFSET_PER_SECOND * (LOOP_TIME + FADE_TIME), FADE_TIME)
+	#scroll_tween.parallel()
+	#scroll_tween.tween_property(noise, ^"offset:z", 0.0, LOOP_TIME)
 	scroll_tween.chain()
 	
 	scroll_tween.tween_callback(random_flip)
