b3a0c1bb9b845669a27869f2b8130800016d3e14
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2026-06-28 05:48:07 -0400

    misc fix: status panel hidden when empty

--- play/hud/hover_panel.gd
+++ play/hud/hover_panel.gd
@@ -6,6 +6,7 @@ const TEMPLATE_STATUS_EFFECT_BUTTON = preload("uid://g8ol80ukqqcc")
 @export var title: RichTextLabel
 @export var key_stats: RichTextLabel
 @export var status_effects_flow_container: HFlowContainer
+@onready var status_panel_container: PanelContainer = %StatusPanelContainer
 
 var status_effects_flow_container_items: Array[Control]
 
@@ -13,6 +14,7 @@ func clear_all() -> void:
 	for child in status_effects_flow_container_items:
 		child.queue_free()
 	status_effects_flow_container_items.clear()
+	status_panel_container.hide()
 		
 	title.text = ""
 	key_stats.text = ""
@@ -46,12 +48,17 @@ func populate_using_actor_data(actor: Actor) -> void:
 	key_stats.text = "[center]"
 	key_stats.append_text("HP: %d / %d" % [actor.health, actor.max_health])
 	
-	for status: Status in actor.get_status_effects():
-		var new_status: TextureButton = TEMPLATE_STATUS_EFFECT_BUTTON.instantiate()
-		new_status.tooltip_text = str(status) + ": " + status.ui_description
-		new_status.texture_normal = status.ui_icon
-		status_effects_flow_container_items.append(new_status)
-		status_effects_flow_container.add_child(new_status)
+	var status_effects = actor.get_status_effects()
+	if status_effects.is_empty():
+		status_panel_container.hide()
+	else:
+		status_panel_container.show()
+		for status: Status in status_effects:
+			var new_status: TextureButton = TEMPLATE_STATUS_EFFECT_BUTTON.instantiate()
+			new_status.tooltip_text = str(status) + ": " + status.ui_description
+			new_status.texture_normal = status.ui_icon
+			status_effects_flow_container_items.append(new_status)
+			status_effects_flow_container.add_child(new_status)
 		
 func populate_using_pickup_data(pickup: PickUp) -> void:
 	clear_all()
