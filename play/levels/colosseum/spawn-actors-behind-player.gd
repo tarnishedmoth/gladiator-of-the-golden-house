a245b5bfd8453e2b696eb30d4318f2a23966650f
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2026-06-29 02:29:16 -0400

    level that spawns enemies when you move

---
+++ play/levels/colosseum/spawn-actors-behind-player.gd
@@ -0,0 +1,39 @@
+extends AIDirector
+
+@export var to_spawn: Array[PackedScene]
+
+@onready var enemies_to_spawn: int = to_spawn.size()
+
+var _enemies_spawned: int = 0
+
+func _ready() -> void:
+	await get_tree().process_frame
+	Level.get_player_main_character().moved.connect(on_player_moved)
+
+func on_player_moved(old_location: Vector2i, new_location: Vector2i) -> void:
+	p("Player moved! %s old, %s new" % [old_location, new_location])
+	if old_location != new_location:
+		spawn_one_at(old_location)
+
+func spawn_one_at(coords: Vector2i) -> void:
+	if not to_spawn: return
+	if to_spawn.is_empty(): return
+	if Level.get_actor_at(coords): return
+	
+	p("Special level spawning actor behind player...")
+	
+	var spawn: Actor = to_spawn.pop_front().instantiate()
+	Juice.fade_in(spawn, Juice.SLOW)
+	add_child(spawn)
+	
+	## set up action
+	add_actor_for_next_turn(spawn)
+	spawn.relocate_root_to_coords(coords)
+
+func has_actors() -> bool: ## Level.gd check objective win condition
+	if _enemies_spawned < enemies_to_spawn:
+		return true
+	elif actors.is_empty():
+		return false
+	else:
+		return true
